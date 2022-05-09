62e82b8d1d5e0b83d6e04a5812de6bb2d9e9376e
gralexx555 <gralex555@gmail.com>
2022-05-09 19:31:10 +0300

    home work 3

---
+++ src/main/java/ru/gb/grigoriev/HomeWork3/HomeWork3.java
@@ -0,0 +1,22 @@
+package ru.gb.grigoriev.HomeWork3;
+
+public class HomeWork3 {
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1 ) {
+                arr[i] = 0;
+            } else {
+                arr[i] = 1;
+
+            }
+            }
+
+        }
+
+
+
+
+
+}
